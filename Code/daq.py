68a1465defc48cb86e2e1f22b35f65d07e90981a
DAQIPN <daqipn@gmail.com>
2024-11-04 15:52:04 -0600

    Update daq.py
    se actualiza la dirección

--- Code/daq.py
+++ Code/daq.py
@@ -13,7 +13,7 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 import threading
 #Path para la gui es establecido
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/home/pi/Tesis/Thesis/Code/GUI/build/assets/frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"/home/pi/Desktop/Tesis/Code/GUI/Homescreen/build/assets/frame0")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
